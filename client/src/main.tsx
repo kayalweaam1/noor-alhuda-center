004a56779a5f076d77fbb6c41b7fac7e97022dfb
kayalweaam1 <kayalweaam1@gmail.com>
2025-10-20 07:28:18 -0400

    Complete OAuth removal - delete all OAuth files and references

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,6 @@ import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
-import { getLoginUrl } from "./const";
 import "./index.css";
 import { registerServiceWorker, setupInstallPrompt } from "./registerSW";
 
@@ -19,7 +18,8 @@ const redirectToLoginIfUnauthorized = (error: unknown) => {
 
   if (!isUnauthorized) return;
 
-  window.location.href = getLoginUrl();
+  // Redirect to login page
+  window.location.href = "/login";
 };
 
 queryClient.getQueryCache().subscribe(event => {
